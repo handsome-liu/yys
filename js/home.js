bc3a05e5ff4703abb673e04e9ea3dc6d4f7d8b73
handsome-liu <735460595@qq.com>
2018-07-24 17:24:07 +0800

    create yys project!

---
+++ js/home.js
@@ -0,0 +1,11 @@
+// import ScrollShow from './ScrollShow';
+
+// new ScrollShow([
+//     $('#newinfo .main .title'),
+//     $('#newinfo .main ul li:nth-of-type(1)'),
+//     $('#newinfo .main ul li:nth-of-type(2)'),
+//     $('#newinfo .main ul li:nth-of-type(3)'),
+//     $('#newinfo .main ul li:nth-of-type(6)'),
+//     $('#newinfo .main ul li:nth-of-type(5)'),
+//     $('#newinfo .main ul li:nth-of-type(4)')
+// ]).init();
